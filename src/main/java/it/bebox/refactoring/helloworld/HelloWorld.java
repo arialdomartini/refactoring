ed7d71211e4a8add526206f353190bb995676859
Arialdo Martini <arialdomartini@gmail.com>
2015-09-10 17:09:56 +0200

    A simple project, with gradle and a dummy test

---
+++ src/main/java/it/bebox/refactoring/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package it.bebox.refactoring.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        World world = new World();
+        System.out.println(world.greet());
+    }
+}
---
+++ src/main/java/it/bebox/refactoring/helloworld/World.java
@@ -0,0 +1,10 @@
+package it.bebox.refactoring.helloworld;
+
+public class World {
+
+    private String greeting = "Hello world!";
+
+    public String greet() {
+        return greeting;
+    }
+}
